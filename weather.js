aa922d47bfdb2b103843eb050bb70709f886f550
Stefan <janjicstefan163@gmail.com>
2018-10-16 23:41:14 +0200

    Display Current Weather Data

--- weather.js
+++ weather.js
@@ -11,13 +11,26 @@ function getWeather() {
         $.ajax({
             url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=63db795291be5ac82fdcc53d99fdf9af',
             type: 'GET',
-            dataType: 'json',
+            dataType: 'jsonp',
             success: function (data) {
-                console.log(data);
-                $('#showWeather').html();
+                var widget = showResults(data);
+
+                $('#showWeather').html(widget);
+
+                $('#city').val('');
             }
         });
     } else {
         $('#error').html("<div>City field cannot be empty</div>");
     }
+}
+
+function showResults(data) {
+    return  "<h3>Current Weather for " + data.name + ", " + data.sys.country + "</h3>" +
+            "<p>Temperature: " + data.main.temp + " &deg;C</p>" +
+            "<p>Pressure: " + data.main.pressure + "</p>" +
+            "<p>Humidity: " + data.main.humidity + "</p>" +
+            "<p>Min Temperature: " + data.main.temp_min + "</p>" +
+            "<p>Max Temperature: " + data.main.temp_max + "</p>" +
+            "<p>Wind Speed: " + data.wind.speed + "</p>";
 }
