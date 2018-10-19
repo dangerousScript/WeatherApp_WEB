8226bfa8eb28f7a68aed28b53279fd1c43f2d757
Stefan <janjicstefan163@gmail.com>
2018-10-19 17:31:37 +0200

    Style Current Weather Data Result Tag

--- weather.js
+++ weather.js
@@ -26,14 +26,14 @@ function getWeather() {
 }
 
 function showResults(data) {
-    return  "<h3>Current Weather for " + data.name + ", " + data.sys.country + "</h3>" +
-            "<p>Weather: " + data.weather[0].main + "</p>" +
-            "<p>Weather Description: " + data.weather[0].description + "</p>" +
-            "<p>Temperature: " + data.main.temp + " &deg;C</p>" +
-            "<p>Pressure: " + data.main.pressure + " hpa</p>" +
-            "<p>Humidity: " + data.main.humidity + " %</p>" +
-            "<p>Min Temperature: " + data.main.temp_min + " &deg;C</p>" +
-            "<p>Max Temperature: " + data.main.temp_max + " &deg;C</p>" +
-            "<p>Wind Speed: " + data.wind.speed + " m/s</p>" +
-            "<p>Wind Direction: " + data.wind.deg + " &deg;</p>";
+    return  "<h2 style='font-weight: bold; font-size: 30px; padding-top: 30px;' class='text-center'>Current Weather for " + data.name + ", " + data.sys.country + "</h2>" +
+            "<h3 style='padding-left: 40px;'><strong>Weather:</strong> " + data.weather[0].main + "</h3>" +
+            "<h3 style='padding-left: 40px;'><strong>Weather Description:</strong> " + data.weather[0].description + "</h3>" +
+            "<h3 style='padding-left: 40px;'><strong>Temperature:</strong> " + data.main.temp + " &deg;C</h3>" +
+            "<h3 style='padding-left: 40px;'><strong>Pressure:</strong> " + data.main.pressure + " hpa</h3>" +
+            "<h3 style='padding-left: 40px;'><strong>Humidity:</strong> " + data.main.humidity + " %</h3>" +
+            "<h3 style='padding-left: 40px;'><strong>Min Temperature:</strong> " + data.main.temp_min + " &deg;C</h3>" +
+            "<h3 style='padding-left: 40px;'><strong>Max Temperature:</strong> " + data.main.temp_max + " &deg;C</h3>" +
+            "<h3 style='padding-left: 40px;'><strong>Wind Speed:</strong> " + data.wind.speed + " m/s</h3>" +
+            "<h3 style='padding-left: 40px; padding-bottom: 30px;'><strong>Wind Direction:</strong> " + data.wind.deg + " &deg;</h3>";
 }
